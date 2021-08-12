94657be5770f11f7656527702dc49f7765afa900
kittybagpipes <sparticus978@gmail.com>
2021-08-12 06:46:29 -0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import ClassTimer from "./components/ClassTimer";
 import Counter from "./components/Counter";
 import DataFetching from "./components/DataFetching";
 import DataFetchingTwo from "./components/DataFetchingTwo";
+import FocusInput from "./components/FocusInput";
+import HookTimer from "./components/HookTimer";
 import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <HookTimer />
     </div>
   );
 }
